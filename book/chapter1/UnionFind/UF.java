8ccf00550b5ad7b4824c2f350d5f0759652b6592
daolanfler <daolanfler@gmail.com>
2022-04-27 08:08:41 +0800

    update

---
+++ book/chapter1/UnionFind/UF.java
@@ -0,0 +1,63 @@
+package book.chapter1.UnionFind;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class UF {
+    private int[] id;
+    private int count;
+
+    public UF(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        validate(p);
+        return id[p];
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n - 1));
+        }
+    }
+
+
+    public void union(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+        if (qID == pID) return;
+
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+}
