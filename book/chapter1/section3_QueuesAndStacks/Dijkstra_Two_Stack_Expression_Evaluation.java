50ffa8ccfcb25b9eee9ec18cecd756e81f9a365b
daolanfler <daolanfler@gmail.com>
2025-04-18 21:50:25 +0800

    comment

--- book/chapter1/section3_QueuesAndStacks/Dijkstra_Two_Stack_Expression_Evaluation.java
+++ book/chapter1/section3_QueuesAndStacks/Dijkstra_Two_Stack_Expression_Evaluation.java
@@ -9,11 +9,17 @@ public class Dijkstra_Two_Stack_Expression_Evaluation {
     public static void main(String[] args) {
         Stack<String> ops = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
+        // 对格式的要求比较苛刻，，，， 要括号，不然不会计算
+        // 还不如看看 Leetcode 的 Calculator 题目
+        // java -cp ./lib/algs4.jar book/chapter1/section3_QueuesAndStacks/Dijkstra_Two_Stack_Expression_Evaluation.java
+
+        // ( ( 1 + 2 ) * 3 ) ctrl + D 
 
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
             if (s.equals("(")) ;
             else if (s.equals("+")) ops.push(s);
+
             else if (s.equals("-")) ops.push(s);
             else if (s.equals("*")) ops.push(s);
             else if (s.equals("/")) ops.push(s);
